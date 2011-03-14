fb8f31fd0a2bd52f23217f2d41362817b8a79107
Peter Ledbrook <peter@cacoethes.co.uk>
2011-03-14 11:37:55 +0000

    BootStrap is now creating an extra task that has multiple paragraphs in its description. I have also rejigged the due dates so that I can demonstrate the different colours for 'past deadline', 'today', 'imminent', etc.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -11,7 +11,7 @@ class BootStrap {
                 summary: "Do 2nd intro screencast",
                 details: "Create the second intro screencast for Grails",
                 status: "Open",
-                deadline: new Date() + 15)
+                deadline: new Date())
         task.addToTags(workTag)
         task.addToTags(homeTag)
         task.save(failOnError: true)
@@ -20,7 +20,7 @@ class BootStrap {
                 summary: "Shopping",
                 details: "Do the grocery shopping - and don't forget the tomatoes!",
                 status: "Open",
-                deadline: new Date() + 3)
+                deadline: new Date() + 15)
         task.addToTags(homeTag)
         task.save(failOnError: true)
         
@@ -28,9 +28,23 @@ class BootStrap {
                 summary: "Finish Grails presentation",
                 details: "The last presentation on Grails & AMQP for SpringOne 2GX needs finishing.",
                 status: "Open",
-                deadline: new Date() + 30)
+                deadline: new Date() - 3)
         task.addToTags(workTag)
-        task.addToTags(homeTag)
+        task.save(failOnError: true)
+        
+        task = new Task(
+                summary: "Another task",
+                details: """\
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod \
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, \
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu \
+fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in \
+culpa qui officia deserunt mollit anim id est laborum.""",
+                status: "Open",
+                deadline: new Date() + 4)
+        task.addToTags(workTag)
         task.save(failOnError: true)
     }
     def destroy = {
